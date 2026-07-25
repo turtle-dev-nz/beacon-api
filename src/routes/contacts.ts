76a68c01044a26b6ec3b83c0d1660d9ace42d47c
Owen Nicholson <theturtledev@gmail.com>
2026-07-26 11:42:09 +1200

    small formatting changes

--- src/routes/contacts.ts
+++ src/routes/contacts.ts
@@ -1,5 +1,5 @@
-import express, { type Request, type Response } from "express";
 import { ContactSource, type ContactCard } from "@prisma/client";
+import express, { type Request, type Response } from "express";
 import prisma from "../config/prisma.js";
 
 const router = express.Router();
